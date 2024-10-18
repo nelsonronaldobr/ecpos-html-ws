c6324d3037b91076ce9831cd2b88ac1a1e9e0563
nelsonronaldobr <nelsonbr862@gmail.com>
2024-10-17 19:59:10 -0500

    feat: push v2

--- api/index.php
+++ api/index.php
@@ -1,18 +1,20 @@
 <?php
 
-$HTML_FILENAME = __DIR__ . '/print-content.html';
+$HTML_FILENAME = sys_get_temp_dir() . '/print-content.html';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['html'])) {
     $html = $_POST['html'];
-    
-    $html .= file_get_contents($HTML_FILENAME);
+
+    if (file_exists($HTML_FILENAME)) {
+        $html .= file_get_contents($HTML_FILENAME);
+    }
 
     $html = str_replace("javascript:window.print()", "", $html);
     $html = str_replace("<body", '<body style="display: flex; flex-direction: column; align-items: center;" ', $html);
     $html = str_replace('class="esc-receipt"', 'class="esc-receipt" style="border-bottom: 3px solid red;" ', $html);
 
     file_put_contents($HTML_FILENAME, $html);
-    
+
     http_response_code(200);
     echo "HTML actualizado correctamente.";
 } else {
@@ -21,7 +23,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['html'])) {
     if (isset($_GET['clear'])) {
         file_put_contents($HTML_FILENAME, '');
     } else {
-        $html = file_get_contents($HTML_FILENAME);
+        if (file_exists($HTML_FILENAME)) {
+            $html = file_get_contents($HTML_FILENAME);
+        }
     }
 
     header("Content-Type: text/html");
